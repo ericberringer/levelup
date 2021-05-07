468b16d1ecd6a6d3bc678ebbaf91f10f21760579
ericberringer <eric.berringer@gmail.com>
2021-05-07 14:26:46 -0500

    all games and events show up on the dom, create a game works.

--- levelupapi/views/game.py
+++ levelupapi/views/game.py
@@ -36,7 +36,7 @@ class GameView(ViewSet):
         # whose `id` is what the client passed as the
         # `gameTypeId` in the body of the request.
         game_type = GameType.objects.get(pk=request.data["gameTypeId"])
-        game.gametype = game_type
+        game.game_type = game_type
 
         # Try to save the new game to the database, then
         # serialize the game instance as JSON, and send the
