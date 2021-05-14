d2f5edb2decc58477ca3b2d186a3889f20ec726e
ericberringer <eric.berringer@gmail.com>
2021-05-14 12:00:18 -0500

    user event count works.

--- levelupapi/views/game.py
+++ levelupapi/views/game.py
@@ -7,7 +7,8 @@ from rest_framework.response import Response
 from rest_framework import serializers
 from rest_framework import status
 from django.db.models import Count
-from levelupapi.models import Game, GameType, Gamer
+from levelupapi.models import Game, GameType, Gamer, gamer
+from django.db.models import Q
 
 
 class GameView(ViewSet):
@@ -124,7 +125,10 @@ class GameView(ViewSet):
             Response -- JSON serialized list of games
         """
         # Get all game records from the database
-        games = Game.objects.annotate(event_count=Count('events'))
+        gamer = Gamer.objects.get(user=request.auth.user)
+        games = Game.objects.annotate(event_count=Count('events'),
+            user_event_count=Count('events',
+            filter=Q(events__organizer=gamer)))
 
         # Support filtering games by type
         #    http://localhost:8000/games?type=1
@@ -146,5 +150,5 @@ class GameSerializer(serializers.ModelSerializer):
     """
     class Meta:
         model = Game
-        fields = ('id', 'maker', 'title', 'number_of_players', 'skill_level', 'game_type', 'event_count')
+        fields = ('id', 'maker', 'title', 'number_of_players', 'skill_level', 'game_type', 'event_count', 'user_event_count')
         depth = 1
