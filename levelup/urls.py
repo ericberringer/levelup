1c141eec5025f8473250367cf5840771bd6a33bc
ericberringer <eric.berringer@gmail.com>
2021-05-27 13:23:45 -0500

    build: Chapter 4 implemented, created a method to handle getting events by user.

--- levelup/urls.py
+++ levelup/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path('login', login_user),
     path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
     path('', include(router.urls)),
+    # This gives us access to the url paths in our levelupreports application
     path('', include('levelupreports.urls')),
 ]
