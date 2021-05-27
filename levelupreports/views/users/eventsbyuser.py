1aa4d00e336df09d4a1534beaae8632a1617a8c6
ericberringer <eric.berringer@gmail.com>
2021-05-27 15:57:21 -0500

    switched sql queries into views via CREATE VIEW for gamesbyuser and eventsbyuser.

--- levelupreports/views/users/eventsbyuser.py
+++ levelupreports/views/users/eventsbyuser.py
@@ -16,22 +16,16 @@ def eventuser_list(request):
             # Query for all events, with related user info.
             db_cursor.execute("""
                 SELECT 
-                    e.id,
-                    e.date,
-                    e.time,
-                    e.event_name,
-                    gr.id,
-                    g.title,
-                    u.id user_id,
-                    u.first_name || ' ' || u.last_name AS full_name
+                    event_id,
+                    date,
+                    time,
+                    event_name,
+                    gamer_id,
+                    title,
+                    user_id,
+                    full_name
                 FROM 
-                    levelupapi_event e
-                JOIN 
-                    levelupapi_gamer gr ON gr.id = e.organizer_id
-                JOIN 
-                    levelupapi_game g ON g.id = e.game_id
-                JOIN 
-                    auth_user u ON u.id = gr.user_id
+                    EVENTS_BY_USER
             """)
 
             dataset = db_cursor.fetchall()
