7fb95cff1ce5214480fb4590e8b326d801296dea
ericberringer <eric.berringer@gmail.com>
2021-05-28 14:02:09 -0500

    created a super user, created player and host groups with specific permissions.

--- level up server.session.sql
+++ level up server.session.sql
@@ -1,17 +1,18 @@
+CREATE VIEW EVENTS_BY_USER AS
 SELECT 
-    e.id EventId,
-    e.date EventDate,
-    e.event_name EventName,
-    e.time EventTime,
-    gr.id GamerId,
-    g.title GameName,
+    e.id event_id,
+    e.date,
+    e.time,
+    e.event_name,
+    gr.id gamer_id,
+    g.title,
     u.id user_id,
-    u.first_name || ' ' || u.last_name AS FullName
-    FROM 
-        levelupapi_event e
-    JOIN 
-        levelupapi_gamer gr ON gr.id = e.id
-    JOIN 
-        levelupapi_game g ON g.id = e.game_id
-    JOIN 
-        auth_user u ON u.id = gr.user_id
+    u.first_name || ' ' || u.last_name AS full_name
+FROM 
+    levelupapi_event e
+JOIN 
+    levelupapi_gamer gr ON gr.id = e.organizer_id
+JOIN 
+    levelupapi_game g ON g.id = e.game_id
+JOIN 
+    auth_user u ON u.id = gr.user_id
